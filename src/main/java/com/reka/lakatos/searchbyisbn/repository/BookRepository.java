57a80c7a2f85ffe1377e27861c376c1ccba7a300
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-10 13:53:09 +0200

    Create BookRepository interface

---
+++ src/main/java/com/reka/lakatos/searchbyisbn/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.reka.lakatos.searchbyisbn.repository;
+
+import com.reka.lakatos.searchbyisbn.document.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends MongoRepository<Book, String> {
+}
