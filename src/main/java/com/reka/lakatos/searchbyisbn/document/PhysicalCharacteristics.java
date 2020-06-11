0e0f8bc5498943ba6fbde2b1815a364b913cc1f0
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-11 11:31:59 +0200

    Add @AllArgsConstructor and @Builder to PhysicalCharacteristics class

--- src/main/java/com/reka/lakatos/searchbyisbn/document/PhysicalCharacteristics.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/document/PhysicalCharacteristics.java
@@ -1,10 +1,14 @@
 package com.reka.lakatos.searchbyisbn.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PhysicalCharacteristics {
     private float width;
     private float height;
