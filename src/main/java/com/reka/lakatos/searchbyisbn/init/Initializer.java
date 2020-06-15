520a0af50e9282b5d7fbd4f63744f9a7ee5f2a24
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-15 12:06:44 +0200

    Delete first two test data

--- src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
@@ -1,16 +1,11 @@
 package com.reka.lakatos.searchbyisbn.init;
 
 import com.reka.lakatos.searchbyisbn.document.Book;
-import com.reka.lakatos.searchbyisbn.document.CoverType;
-import com.reka.lakatos.searchbyisbn.document.PhysicalCharacteristics;
 import com.reka.lakatos.searchbyisbn.repository.BookRepository;
 import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.HashSet;
 
 @Component
 public class Initializer implements CommandLineRunner {
@@ -23,26 +18,6 @@ public class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Book chilrdern = Book.builder()
-                .title("The children").build();
-        bookRepository.save(chilrdern);
-
-        Book simpleBook = Book.builder()
-                .author("Test Test")
-                .title("The test book")
-                .subtitle("Something")
-                .authorNotice("Working Db")
-                .publisher("RVL CODING")
-                .yearOfRelease("cop. 2020")
-                .isbn("000000000000")
-                .contributors(new HashSet<>(Arrays.asList("Me", "Me", "Robin Hobb")))
-                .booksIdsWhatPartThisBook(new HashSet<>(Arrays.asList(chilrdern.getId())))
-                .physicalCharacteristics(PhysicalCharacteristics.builder().coverType(CoverType.HARDCORE).build())
-                .build();
-
-        bookRepository.save(simpleBook);
-        chilrdern.setBooksIdsWhatContainThisBook(new HashSet<>(Arrays.asList(simpleBook.getId())));
-        bookRepository.save(chilrdern);
 
         Book book = Book.builder()
                 .author("Robin Hobb")
