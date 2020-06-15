e05161fe143ea66f210830b451aa74a18066a260
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-15 14:21:44 +0200

    Change /save endpoint implementation in BookController

--- src/main/java/com/reka/lakatos/searchbyisbn/controller/BookController.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/controller/BookController.java
@@ -2,6 +2,7 @@ package com.reka.lakatos.searchbyisbn.controller;
 
 import com.reka.lakatos.searchbyisbn.document.Book;
 import com.reka.lakatos.searchbyisbn.service.BookService;
+import com.reka.lakatos.searchbyisbn.service.util.RegistryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +16,10 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    /*
     @PostMapping("/save")
-    public ResponseEntity<String> saveABook(@RequestBody Book book) {
-        boolean result = bookService.saveBook(book);
-        if (result) {
-            return new ResponseEntity<>("Save was successful", HttpStatus.OK);
-        }
-        return new ResponseEntity<>("The book is already registered with this ISBN code", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<RegistryResult> saveABook(@RequestBody Book book) {
+        RegistryResult result = bookService.saveBook(book);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
-     */
 
 }
