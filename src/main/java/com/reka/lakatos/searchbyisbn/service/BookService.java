8ccafcab69b2cdd65629259d77f757e9b93a6028
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-15 14:21:09 +0200

    Change saveBook implementation in BookService

--- src/main/java/com/reka/lakatos/searchbyisbn/service/BookService.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/service/BookService.java
@@ -2,6 +2,8 @@ package com.reka.lakatos.searchbyisbn.service;
 
 import com.reka.lakatos.searchbyisbn.document.Book;
 import com.reka.lakatos.searchbyisbn.repository.BookRepository;
+import com.reka.lakatos.searchbyisbn.service.util.BookRegistry;
+import com.reka.lakatos.searchbyisbn.service.util.RegistryResult;
 import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     @Autowired
-    private ISBNValidator isbnValidator;
+    private BookRegistry bookRegistry;
 
-    @Autowired
-    private BookRepository bookRepository;
+    public RegistryResult saveBook(Book book) {
+        return bookRegistry.registBook(book);
+    }
+
 
 
 
