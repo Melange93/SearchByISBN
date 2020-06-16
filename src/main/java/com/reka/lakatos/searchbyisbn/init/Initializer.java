67687ca706e094a9c3b0bf80ed99f60fe214156f
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-16 11:30:31 +0200

    Change @Component to @Configuration in Initializer class

--- src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
@@ -1,13 +1,16 @@
 package com.reka.lakatos.searchbyisbn.init;
 
+import com.reka.lakatos.searchbyisbn.crawler.BookCrawler;
+import com.reka.lakatos.searchbyisbn.crawler.metropolitan_ervin_szabo_library.MetropolitanErvinSzaboLibraryCrawler;
 import com.reka.lakatos.searchbyisbn.document.Book;
 import com.reka.lakatos.searchbyisbn.repository.BookRepository;
 import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class Initializer implements CommandLineRunner {
 
     private BookRepository bookRepository;
