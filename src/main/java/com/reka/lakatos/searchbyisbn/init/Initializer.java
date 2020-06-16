a3c890496c371c024570f5abd60b2a4a81c81899
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-16 15:51:15 +0200

    Call bookCrawler.getNextBooks() method in Initializer class run mathod

--- src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
@@ -18,7 +18,7 @@ public class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        bookCrawler.getNextBooks();
     }
 
     @Bean
