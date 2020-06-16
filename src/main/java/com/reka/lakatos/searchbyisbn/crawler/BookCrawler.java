bc9909ee1a13aee614033c8a28b620f690c47656
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-16 10:36:16 +0200

    Create BookCrawler interface

---
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/BookCrawler.java
@@ -0,0 +1,11 @@
+package com.reka.lakatos.searchbyisbn.crawler;
+
+import com.reka.lakatos.searchbyisbn.document.Book;
+
+import java.util.List;
+
+public interface BookCrawler {
+
+    List<Book> getNextBooks();
+
+}
