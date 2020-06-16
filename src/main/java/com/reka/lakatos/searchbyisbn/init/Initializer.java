0a0a000bc2409fc0b7873e2f9a96c5896f7092c1
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-16 11:32:47 +0200

    Change BookRepository field to BookCrawler field

--- src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/init/Initializer.java
@@ -2,21 +2,18 @@ package com.reka.lakatos.searchbyisbn.init;
 
 import com.reka.lakatos.searchbyisbn.crawler.BookCrawler;
 import com.reka.lakatos.searchbyisbn.crawler.metropolitan_ervin_szabo_library.MetropolitanErvinSzaboLibraryCrawler;
-import com.reka.lakatos.searchbyisbn.document.Book;
-import com.reka.lakatos.searchbyisbn.repository.BookRepository;
 import org.apache.commons.validator.routines.ISBNValidator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class Initializer implements CommandLineRunner {
 
-    private BookRepository bookRepository;
+    private BookCrawler bookCrawler;
 
-    public Initializer(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public Initializer() {
+        this.bookCrawler = new MetropolitanErvinSzaboLibraryCrawler();
     }
 
     @Override
