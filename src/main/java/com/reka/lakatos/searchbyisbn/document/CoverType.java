6f55b9e54e242f42424d8bf329fa10f885d5053a
Melange93 <lakatos.reka.veronika@gmail.com>
2020-06-22 15:26:34 +0200

    Add values to CoverType enum

--- src/main/java/com/reka/lakatos/searchbyisbn/document/CoverType.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/document/CoverType.java
@@ -1,5 +1,28 @@
 package com.reka.lakatos.searchbyisbn.document;
 
 public enum CoverType {
-    HARDCORE, PAPERBACK, BOUND, DIGITAL
+    HARDCORE("kötött"), PAPERBACK("fűzött"), DIGITAL("");
+
+    private String hunName;
+
+    CoverType(String hunName) {
+        this.hunName = hunName;
+    }
+
+    public String getHunName() {
+        return hunName;
+    }
+
+    public static CoverType findTypeByName(String hunName){
+        if (hunName.isBlank()) {
+            throw new NullPointerException("Cover type can't be empty!");
+        }
+
+        for(CoverType v : values()){
+            if( v.getHunName().equals(hunName)){
+                return v;
+            }
+        }
+        return null;
+    }
 }
