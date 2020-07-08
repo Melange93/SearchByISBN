68e9a859336d646afa8fe0c74f2f51379e2b8488
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-08 16:12:53 +0200

    Start implementing getBookListLinks method

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/Crawler.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/Crawler.java
@@ -2,35 +2,68 @@ package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary;
 
 import com.reka.lakatos.searchbyisbn.crawler.BookCrawler;
 import com.reka.lakatos.searchbyisbn.document.Book;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 @ConditionalOnProperty(name = "crawler.book-crawler", havingValue = "szechenyi")
 public class Crawler implements BookCrawler {
 
+    private final SessionIdFactory sessionIdFactory;
+
+    private static final String SERVER_1_URL = "http://nektar1.oszk.hu/LVbin/LibriVision/lv_scan.html";
+    private static final String SERVER_2_URL = "http://nektar2.oszk.hu/LVbin/LibriVision/lv_scan.html";
+    private static final String SERVER_1_NAME = "nektar1";
+    private static final String SERVER_2_NAME = "nektar2";
+
+    private String currentServerUrl;
+    private String currentServerSessionId;
+
     @Override
     public List<Book> getNextBooks() {
+        System.out.println(sessionIdFactory.getServerAndSessionId());
+        System.out.println(getBookListLinks());
+        return null;
+    }
+
+    private List<String> getBookListLinks() {
+
+        Optional<Map<String, String>> sessionId = sessionIdFactory.getServerAndSessionId();
+        if (sessionId.isPresent()) {
+            Map<String, String> serverSessionId = sessionId.get();
+            if (serverSessionId.containsKey(SERVER_1_NAME)) {
+                currentServerUrl = SERVER_1_URL;
+                currentServerSessionId = serverSessionId.get(SERVER_1_NAME);
+            }
+            currentServerUrl = SERVER_2_URL;
+            currentServerSessionId = serverSessionId.get(SERVER_2_NAME);
+        }
+
+        System.out.println(currentServerSessionId);
+        System.out.println(currentServerUrl);
+
+
         try {
-            log.info("Széchenyi");
-            Document document = Jsoup.connect("http://nektar1.oszk.hu/LVbin/LibriVision/lv_libri_url_db_v2.html?USER_LOGIN=Nektar_LV_user&USER_PASSWORD=Nektar&LanguageCode=hu&CountryCode=hu&HtmlSetCode=default&lv_action=LV_Login&HTML_SEARCH_TYPE=SIMPLE&DIRECT_SEARCH_TYPE=BK&DIRECT_SEARCH_TERM=978-963-0&DB_ID=2").get();
+            Document document = Jsoup.connect(currentServerUrl)
+                    .requestBody("SESSION_ID=" + currentServerSessionId + "&lv_action=LV_Scan&new_scan=-1&SCAN_TERM=978-963-0&SCAN_USE=BN&SCAN_PREFERRED_POSITION_IN_RESPONSE=2&SCAN_NUMBER_OF_TERMS_REQUESTED=10&SCAN_STEP_SIZE=0")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .post();
 
-            Elements elementsByAttributeValueStarting = document.getElementsByAttributeValueStarting("name", "SESSION_ID");
-            System.out.println(elementsByAttributeValueStarting);
-            System.out.println(elementsByAttributeValueStarting.get(0).attr("value"));
-
-            return null;
+            System.out.println(document);
         } catch (IOException e) {
             log.error(String.valueOf(e));
-            return null;
         }
+        return null;
     }
 }
