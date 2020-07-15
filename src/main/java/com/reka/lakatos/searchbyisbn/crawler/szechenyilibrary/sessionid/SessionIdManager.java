e9d6f5ce4ee4d3c6a4890fd8bf3123de3aff344b
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-15 12:26:16 +0200

    Refactor SessionIdManager class and reimplement setCurrentServerUrlAndCurrentServerSessionId method in Crawler Delete getServerAndSessionId, getServer, getSessionId method and SERVERS_URL field Reimplement getActivatedServerAndSessionId method

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/SessionIdManager.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/SessionIdManager.java
@@ -1,40 +1,24 @@
 package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid;
 
+import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.model.Session;
+import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.sessionidcreator.SessionIdCreator;
 import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.webclient.WebClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SessionIdManager {
     private final WebClient webClient;
+    private final SessionIdCreator sessionIdCreator;
 
-    private static final String[] SERVERS_URL = {
-            "http://nektar1.oszk.hu/LVbin/LibriVision/",
-            "http://nektar2.oszk.hu/LVbin/LibriVision/"};
-
-    public Map.Entry<String, String> getActivatedServerAndSessionId() {
+    public Session getActivatedServerAndSessionId() {
         try {
-            Optional<Map.Entry<String, String>> serverAndSessionId = getServerAndSessionId();
-
-            if (serverAndSessionId.isPresent()) {
-                String serverUrl = serverAndSessionId.get().getKey();
-                String sessionId = serverAndSessionId.get().getValue();
-                webClient.activateSessionId(serverUrl, sessionId);
-                return serverAndSessionId.get();
-            }
-            // ToDo create unique exception
-            throw new RuntimeException(
-                    getClass().getSimpleName() +
-                            " can't provide an active Session ID");
+            Session session = sessionIdCreator.getNewSession();
+            webClient.activateSessionId(session.getServerUrl(), session.getId());
+            return session;
         } catch (Exception e) {
             // ToDo create unique exception
             throw new RuntimeException(
@@ -42,32 +26,5 @@ public class SessionIdManager {
                             " can't provide an active Session ID because of "
                             + e);
         }
-    }
-
-    private Optional<Map.Entry<String, String>> getServerAndSessionId() {
-        final Document document = webClient.getServerAndSessionIdDocument();
-        final Optional<String> server = getServer(document);
-        final Optional<String> sessionId = getSessionId(document);
-
-        return server.isPresent() && sessionId.isPresent() ?
-                Optional.of(Map.entry(server.get(), sessionId.get())) :
-                Optional.empty();
-    }
-
-    private Optional<String> getServer(final Document document) {
-        return Arrays.stream(SERVERS_URL)
-                .filter(serverUrl -> document
-                        .getElementsByAttributeValueMatching("action", serverUrl)
-                        .size() == 1)
-                .findFirst();
-    }
-
-    private Optional<String> getSessionId(final Document document) {
-        Elements getSessionIdElement =
-                document.getElementsByAttributeValueStarting("name", "SESSION_ID");
-
-        return getSessionIdElement.size() == 1 ?
-                Optional.of(getSessionIdElement.first().attr("value")) :
-                Optional.empty();
     }
 }
