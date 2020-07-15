d26a2d31047bbb9f553444794144060332441aa5
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-15 16:06:01 +0200

    Delete sessionid/webclient package

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/webclient/exception/SessionActivationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.webclient.exception;
-
-public class SessionActivationFailedException extends RuntimeException {
-    public SessionActivationFailedException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/webclient/exception/SessionDocumentFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.webclient.exception;
-
-public class SessionDocumentFailureException extends RuntimeException {
-    public SessionDocumentFailureException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
