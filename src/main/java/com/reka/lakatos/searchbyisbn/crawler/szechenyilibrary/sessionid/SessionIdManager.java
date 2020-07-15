995b81f6eb20416656bf834a5bc1ad747b28f1a4
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-15 14:03:31 +0200

    Delete SessionIdCreator class and move the getNewSession implementation into SessionIdManager getActivatedServerAndSessionId method

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/SessionIdManager.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/SessionIdManager.java
@@ -1,22 +1,27 @@
 package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid;
 
+import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.documentreader.DocumentReader;
 import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.model.Session;
-import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.sessionidcreator.SessionIdCreator;
 import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.webclient.WebClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SessionIdManager {
+
     private final WebClient webClient;
-    private final SessionIdCreator sessionIdCreator;
+    private final DocumentReader documentReader;
 
     public Session getActivatedServerAndSessionId() {
         try {
-            Session session = sessionIdCreator.getNewSession();
+            final Document document = webClient.getServerAndSessionIdDocument();
+            final String serverUrl = documentReader.getServerUrl(document);
+            final String sessionId = documentReader.getSessionId(document);
+            Session session = new Session(serverUrl, sessionId);
             webClient.activateSessionId(session.getServerUrl(), session.getId());
             return session;
         } catch (Exception e) {
