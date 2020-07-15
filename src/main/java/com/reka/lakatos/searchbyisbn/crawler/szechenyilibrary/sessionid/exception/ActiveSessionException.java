fe749a3cff04362ee2766dd6ba217270df003d64
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-15 16:07:12 +0200

    Rename ActiveSessionException to SessionException

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/sessionid/exception/ActiveSessionException.java
+++
@@ -1,7 +0,0 @@
-package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.exception;
-
-public class ActiveSessionException extends RuntimeException {
-    public ActiveSessionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
