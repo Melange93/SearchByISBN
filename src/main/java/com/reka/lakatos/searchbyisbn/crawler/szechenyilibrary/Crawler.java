b5d5857db75aa5e028ff62e4a0716942bc545107
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-17 16:44:47 +0200

    Reimplement setCurrentServerUrlAndCurrentServerSessionId method in Crawler in szechenyilibary package

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/Crawler.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/Crawler.java
@@ -1,7 +1,8 @@
 package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary;
 
 import com.reka.lakatos.searchbyisbn.crawler.BookCrawler;
-import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.sessionid.SessionIdManager;
+import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.session.Session;
+import com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.session.SessionManager;
 import com.reka.lakatos.searchbyisbn.document.Book;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -58,8 +58,8 @@ public class Crawler implements BookCrawler {
     }
 
     private void setCurrentServerUrlAndCurrentServerSessionId() {
-        Map.Entry<String, String> serverUrlAndSessionId = sessionIdFactory.getActivatedServerAndSessionId();
-        currentServerUrl = serverUrlAndSessionId.getKey() + "lv_scan.html";
-        currentServerSessionId = serverUrlAndSessionId.getValue();
+        Session session = sessionManager.getActiveSession();
+        currentServerUrl = session.getServerUrl() + "lv_scan.html";
+        currentServerSessionId = session.getId();
     }
 }
