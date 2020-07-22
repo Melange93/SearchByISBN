785cbcd355c9724f34195b12794ad8b223ee2cc1
Melange93 <lakatos.reka.veronika@gmail.com>
2020-07-22 15:37:46 +0200

    Implement edition saving, but not test it yet

--- src/main/java/com/reka/lakatos/searchbyisbn/service/util/BookRegistry.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/service/util/BookRegistry.java
@@ -1,11 +1,13 @@
 package com.reka.lakatos.searchbyisbn.service.util;
 
 import com.reka.lakatos.searchbyisbn.document.Book;
+import com.reka.lakatos.searchbyisbn.document.Edition;
 import com.reka.lakatos.searchbyisbn.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -35,12 +37,70 @@ public class BookRegistry {
     }
 
     private void updateEmptyFields(Book fromDb, Book newOne) {
+        if (fromDb.getEditions() != null && newOne.getEditions() != null) {
+            updateEdition(fromDb, newOne);
+        }
         if (fromDb.getAuthor() == null && newOne.getAuthor() != null) {
             fromDb.setAuthor(newOne.getAuthor());
         }
         if (fromDb.getTitle() == null && newOne.getTitle() != null) {
             fromDb.setTitle(newOne.getTitle());
         }
+        if (fromDb.getEditions() == null && newOne.getEditions() != null) {
+            fromDb.setEditions(newOne.getEditions());
+        }
+        if (fromDb.getCoverType() == null && newOne.getCoverType() != null) {
+            fromDb.setCoverType(newOne.getCoverType());
+        }
+    }
+
+    private void updateEdition(Book fromDb, Book newOne) {
+        Optional<Edition> first = newOne.getEditions().stream().findFirst();
+        if (first.isPresent()) {
+            Edition newOneEdition = first.get();
+            int editionIndex = findEdition(fromDb.getEditions(),
+                    newOneEdition.getEditionNumber(),
+                    newOneEdition.getYearOfRelease());
+
+            if (editionIndex > -1) {
+                Edition updatedEdition = updateEditionEmptyField(fromDb.getEditions().get(editionIndex), newOneEdition);
+                fromDb.getEditions().set(editionIndex, updatedEdition);
+            }
+        }
+    }
+
+    private int findEdition(List<Edition> fromDbEditions, int editionNumber, int yearsOfRelease) {
+        if (editionNumber == 0 && yearsOfRelease == 0) {
+            return -1;
+        }
+
+        if (editionNumber != 0 && yearsOfRelease != 0) {
+            Optional<Edition> found = fromDbEditions.stream()
+                    .filter(edition -> edition.getEditionNumber() == editionNumber
+                            && edition.getYearOfRelease() == yearsOfRelease)
+                    .findFirst();
+            if (found.isPresent()) {
+                return fromDbEditions.indexOf(found.get());
+            }
+        }
+
+        if (yearsOfRelease == 0) {
+            Optional<Edition> found = fromDbEditions.stream()
+                    .filter(edition -> edition.getEditionNumber() == editionNumber)
+                    .findFirst();
+            if (found.isPresent()) {
+                return fromDbEditions.indexOf(found.get());
+            }
+        }
+
+        return fromDbEditions.stream()
+                .filter(edition -> edition.getYearOfRelease() == yearsOfRelease)
+                .findFirst()
+                .map(fromDbEditions::indexOf)
+                .orElse(-1);
+    }
+
+    private Edition updateEditionEmptyField(Edition fromDb, Edition newOne) {
         if (fromDb.getYearOfRelease() == 0 && newOne.getYearOfRelease() != 0) {
             fromDb.setYearOfRelease(newOne.getYearOfRelease());
         }
@@ -53,9 +113,7 @@ public class BookRegistry {
         if (fromDb.getPageNumber() == 0 && newOne.getPageNumber() != 0) {
             fromDb.setPageNumber(newOne.getPageNumber());
         }
-        if (fromDb.getCoverType() == null && newOne.getCoverType() != null) {
-            fromDb.setCoverType(newOne.getCoverType());
-        }
-    }
 
+        return fromDb;
+    }
 }
