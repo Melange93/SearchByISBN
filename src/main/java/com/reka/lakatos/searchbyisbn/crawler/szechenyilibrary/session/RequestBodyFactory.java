34451db3e269814bb54de72288b7e20879ad7d6d
Melange93 <lakatos.reka.veronika@gmail.com>
2020-08-03 11:15:33 +0200

    Create RequestBodyFactory class

---
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/session/RequestBodyFactory.java
@@ -0,0 +1,23 @@
+package com.reka.lakatos.searchbyisbn.crawler.szechenyilibrary.session;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestBodyFactory {
+
+    private static final int SEARCHING_BOOK_NUMBER = 50;
+
+    public String getSearchingBody(final String currentServerSessionId) {
+        return "SESSION_ID="
+                + currentServerSessionId
+                + "&lv_action=LV_Scan"
+                + "&new_scan=-1"
+                + "&SCAN_TERM=978-963-0"
+                + "&SCAN_USE=BN"
+                + "&SCAN_PREFERRED_POSITION_IN_RESPONSE=2"
+                + "&SCAN_NUMBER_OF_TERMS_REQUESTED="
+                + SEARCHING_BOOK_NUMBER
+                + "&SCAN_STEP_SIZE=0";
+    }
+
+}
