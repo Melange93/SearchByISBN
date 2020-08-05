e2d3278163eb826c2d4a2e4c071d216af2a015ba
Melange93 <lakatos.reka.veronika@gmail.com>
2020-08-05 16:10:30 +0200

    Create getNextPageBody method in RequestBodyFactory

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/factory/RequestBodyFactory.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/szechenyilibrary/factory/RequestBodyFactory.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class RequestBodyFactory {
 
     private static final int SEARCHING_BOOK_NUMBER = 10;
+    private static final int NUMBER_OF_RECORDS = 50;
 
     public String getSearchingBody(final String currentServerSessionId) {
         return "SESSION_ID="
@@ -20,4 +21,18 @@ public class RequestBodyFactory {
                 + "&SCAN_STEP_SIZE=0";
     }
 
+    public String getNextPageBody(final String currentServerSessionId, final String scanTerm) {
+        return "SESSION_ID=" +
+                currentServerSessionId +
+                "&lv_action=LV_Scan" +
+                "&new_scan=-1" +
+                "&SCAN_TERM=" +
+                scanTerm +
+                "&SCAN_USE_ATTRIBUTE=7" +
+                "&SCAN_USE=BN" +
+                "&SCAN_PREFERRED_POSITION_IN_RESPONSE=10" +
+                "&SCAN_NUMBER_OF_TERMS_REQUESTED=" +
+                NUMBER_OF_RECORDS +
+                "&SCAN_STEP_SIZE=0";
+    }
 }
