da51326a919dd0db46fa15dc1f0639be28ba1622
Melange93 <lakatos.reka.veronika@gmail.com>
2020-08-18 17:01:26 +0200

    Create getCookies method in WebClient interface and implement it in JsoupWebClient class

--- src/main/java/com/reka/lakatos/searchbyisbn/webdocument/WebClient.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/webdocument/WebClient.java
@@ -1,8 +1,12 @@
 package com.reka.lakatos.searchbyisbn.webdocument;
 
+import java.util.Map;
+
 public interface WebClient {
 
     WebDocument sendGetRequest(String baseUrl);
 
     WebDocument sendPostRequest(String baseUrl, String requestBody);
+
+    Map<String, String> getCookies(String baseUrl);
 }
--- src/main/java/com/reka/lakatos/searchbyisbn/webdocument/jsoup/JsoupWebClient.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/webdocument/jsoup/JsoupWebClient.java
@@ -3,10 +3,12 @@ package com.reka.lakatos.searchbyisbn.webdocument.jsoup;
 import com.reka.lakatos.searchbyisbn.webdocument.WebClient;
 import com.reka.lakatos.searchbyisbn.webdocument.WebDocument;
 import com.reka.lakatos.searchbyisbn.webdocument.exception.WebClientException;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class JsoupWebClient implements WebClient {
@@ -31,6 +33,22 @@ public class JsoupWebClient implements WebClient {
         } catch (IOException e) {
             throw new WebClientException("Failed to reach the url: " + baseUrl +
                     " with this request body: " + requestBody, e);
+        }
+    }
+
+    @Override
+    public Map<String, String> getCookies(String baseUrl) {
+        try {
+            Connection.Response res = Jsoup
+                    .connect(baseUrl)
+                    .method(Connection.Method.GET)
+                    .execute();
+            return res.cookies();
+        } catch (IOException e) {
+            throw new WebClientException(
+                    "Failed to get the cookies from this url: " + baseUrl + " Request method is GET",
+                    e
+            );
         }
     }
 }
