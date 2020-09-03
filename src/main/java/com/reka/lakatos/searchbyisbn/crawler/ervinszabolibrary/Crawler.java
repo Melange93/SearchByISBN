cbfe775c227d86ecb8cd3b46497440e134416713
Melange93 <lakatos.reka.veronika@gmail.com>
2020-09-03 12:26:31 +0200

    Correct scrolling when too much result have

--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/ervinszabolibrary/Crawler.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/ervinszabolibrary/Crawler.java
@@ -29,25 +29,25 @@ public class Crawler implements BookCrawler {
     private static final String NAME = "Ervin Szabo Library";
     private static final String[] ISBN_GROUPS = {"978963", "978615", "963"};
     private static final String PAGE_SIZE = "10";
-    private static final int MAX_NUMBER = 9;
+    private static final int MAX_NUMBER = 99;
 
-    private int page = 0;
-    private int isbnSeventhNumber = 0;
+    private int resultNumber;
+    private int page = 5;
+    private int isbnSeventhNumber = 99;
     private int ISBNGroupIndex = 2;
 
     @Override
     public List<Book> getNextBooks() {
         try {
-            log.info("Start crawling: {}, Page number: {}, Searching ISBN: {}",
-                    NAME, page + 1, ISBN_GROUPS[ISBNGroupIndex] + isbnSeventhNumber);
-
-            List<Book> books = getCrawledBooks();
-            booksPageScrolling(books);
-
-            System.out.println(books);
             if (ISBNGroupIndex == ISBN_GROUPS.length) {
+                System.out.println("here");
                 return null;
             }
+
+            String lastNumber = calculateSearchingLastNumber();
+            printSearchingInformation(lastNumber);
+            List<Book> books = getCrawledBooks(lastNumber);
+            booksPageScrolling();
 
             return books;
         } catch (Exception e) {
@@ -59,15 +59,28 @@ public class Crawler implements BookCrawler {
         }
     }
 
-    private List<Book> getCrawledBooks() {
+    private void printSearchingInformation(String lastNumber) {
+        log.info("Start crawling: {}, Page number: {}, Searching ISBN: {}",
+                NAME, page + 1, ISBN_GROUPS[ISBNGroupIndex] + lastNumber);
+    }
+
+    private String calculateSearchingLastNumber() {
+        String lastNumber = String.valueOf(isbnSeventhNumber);
+        if (isbnSeventhNumber < 10) {
+            lastNumber = "0" + isbnSeventhNumber;
+        }
+        return lastNumber;
+    }
+
+    private List<Book> getCrawledBooks(String lastNumber) {
+
+        String isbnSearchingUrl = urlFactory.createISBNSearchingUrl(
+                page,
+                ISBN_GROUPS[ISBNGroupIndex] + lastNumber,
+                PAGE_SIZE
+        );
         final Map<String, String> booksPropertiesPageUrlInformation =
-                getInformationForBookPropertiesUrl(
-                        urlFactory.createISBNSearchingUrl(
-                                page,
-                                ISBN_GROUPS[ISBNGroupIndex] + isbnSeventhNumber,
-                                PAGE_SIZE
-                        )
-                );
+                getInformationForBookPropertiesUrl(isbnSearchingUrl);
 
         return crawledBooksCreator.getCrawledBooks(booksPropertiesPageUrlInformation);
     }
@@ -75,7 +88,7 @@ public class Crawler implements BookCrawler {
     private Map<String, String> getInformationForBookPropertiesUrl(String ISBNSearchingUrl) {
         try {
             final WebDocument booksPage = webClient.sendGetRequest(ISBNSearchingUrl);
-
+            resultNumber = documentReaderFacade.getResultNumber(booksPage);
             return documentReaderFacade.getInformationForBookPropertiesPage(booksPage);
         } catch (WebClientException e) {
             throw new BookListDownloadException("Unable to download the list book page! Url: " + ISBNSearchingUrl, e);
@@ -86,28 +99,20 @@ public class Crawler implements BookCrawler {
         page++;
     }
 
-    private void booksPageScrolling(List<Book> books) {
-        page++;
-
-        if (isCrawlerFinishedThisSeventhISBNNumberCrawling(books)) {
-            page = 0;
-            isbnSeventhNumber++;
+    private void booksPageScrolling() {
+        int remainingBooks = resultNumber - ((page + 1) * Integer.parseInt(PAGE_SIZE));
+        if (remainingBooks > 0) {
+            page++;
         }
 
-        if (isCrawlerFinishedThisISBNGroup(books)) {
+        if (remainingBooks <= 0) {
             page = 0;
-            isbnSeventhNumber = 0;
-            ISBNGroupIndex++;
+            if (isbnSeventhNumber >= MAX_NUMBER) {
+                isbnSeventhNumber = 0;
+                ISBNGroupIndex++;
+            } else {
+                isbnSeventhNumber++;
+            }
         }
-    }
-
-    private boolean isCrawlerFinishedThisSeventhISBNNumberCrawling(List<Book> books) {
-        return books.size() == 0
-                && isbnSeventhNumber <= MAX_NUMBER;
-    }
-
-    private boolean isCrawlerFinishedThisISBNGroup(List<Book> books) {
-        return books.size() == 0
-                && isbnSeventhNumber > MAX_NUMBER;
     }
 }
--- src/main/java/com/reka/lakatos/searchbyisbn/crawler/ervinszabolibrary/documentreader/DocumentReaderFacade.java
+++ src/main/java/com/reka/lakatos/searchbyisbn/crawler/ervinszabolibrary/documentreader/DocumentReaderFacade.java
@@ -3,10 +3,14 @@ package com.reka.lakatos.searchbyisbn.crawler.ervinszabolibrary.documentreader;
 import com.reka.lakatos.searchbyisbn.crawler.ervinszabolibrary.documentreader.facade.BookPropertiesPageReader;
 import com.reka.lakatos.searchbyisbn.crawler.ervinszabolibrary.documentreader.facade.BooksPageReader;
 import com.reka.lakatos.searchbyisbn.webdocument.WebDocument;
+import com.reka.lakatos.searchbyisbn.webdocument.WebElement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +27,15 @@ public class DocumentReaderFacade {
         return bookPropertiesPageReader.getBookProperties(bookPropertiesPage);
     }
 
+    public int getResultNumber(WebDocument bookListPage) {
+        Optional<WebElement> searchingResult = bookListPage.select(".result-size").stream().findFirst();
+        if (searchingResult.isPresent()) {
+            Matcher matcher = Pattern.compile("\\d+").matcher(searchingResult.get().text());
+            if (matcher.find()) {
+                return Integer.parseInt(matcher.group());
+            }
+        }
+        return 0;
+    }
 
 }
