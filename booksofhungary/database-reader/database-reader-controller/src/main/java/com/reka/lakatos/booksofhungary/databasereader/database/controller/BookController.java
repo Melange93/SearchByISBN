98d281325747df6a439cd60f207f5833637e1261
Melange93 <lakatos.reka.veronika@gmail.com>
2021-01-13 14:35:50 +0100

    Refactor BookController

--- booksofhungary/database-reader/database-reader-controller/src/main/java/com/reka/lakatos/booksofhungary/databasereader/database/controller/BookController.java
+++ booksofhungary/database-reader/database-reader-controller/src/main/java/com/reka/lakatos/booksofhungary/databasereader/database/controller/BookController.java
@@ -5,7 +5,6 @@ import com.reka.lakatos.booksofhungary.databasereader.database.domain.BookRespon
 import com.reka.lakatos.booksofhungary.databasereader.database.domain.Edition;
 import com.reka.lakatos.booksofhungary.databasereader.database.domain.EditionResponse;
 import com.reka.lakatos.booksofhungary.databasereader.database.service.BookService;
-import com.reka.lakatos.booksofhungary.databasereader.database.domain.CoverTypeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @CrossOrigin
@@ -23,40 +21,27 @@ public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping("/")
-    public String getHelloWord() {
-        return "Hello book shelf";
-    }
-
     @GetMapping("/searchbook")
     public BookResponse getBookByISBN(@RequestParam String isbn) {
-        System.out.println(isbn);
-        Optional<Book> bookByISBN = bookService.getBookByISBN(isbn);
-        if (bookByISBN.isPresent()) {
-            Book book = bookByISBN.get();
-            List<Edition> editions = book.getEditions();
-            List<EditionResponse> editionResponses = editions.stream().map(edition ->
-                    EditionResponse.builder()
-                            .contributors(edition.getContributors())
-                            .editionNumber(edition.getEditionNumber())
-                            .pageNumber(edition.getPageNumber())
-                            .thickness(edition.getThickness())
-                            .yearOfRelease(edition.getYearOfRelease())
-                            .build()
-            ).collect(Collectors.toList());
+        Book book = bookService.getBookByISBN(isbn);
+        List<Edition> editions = book.getEditions();
+        List<EditionResponse> editionResponses = editions.stream()
+                .map(edition ->
+                        EditionResponse.builder()
+                                .contributors(edition.getContributors())
+                                .editionNumber(edition.getEditionNumber())
+                                .pageNumber(edition.getPageNumber())
+                                .thickness(edition.getThickness())
+                                .yearOfRelease(edition.getYearOfRelease())
+                                .build()
+                ).collect(Collectors.toList());
 
-            CoverTypeResponse coverTypeResponse = CoverTypeResponse.valueOf(book.getCoverType().getHunName());
-
-
-            return BookResponse.builder()
-                    .isbn(book.getIsbn())
-                    .author(book.getAuthor())
-                    .coverTypeResponse(coverTypeResponse)
-                    .editions(editionResponses)
-                    .publisher(book.getPublisher())
-                    .title(book.getTitle())
-                    .build();
-        }
-        throw new RuntimeException("No such a book");
+        return BookResponse.builder()
+                .isbn(book.getIsbn())
+                .author(book.getAuthor())
+                .editions(editionResponses)
+                .publisher(book.getPublisher())
+                .title(book.getTitle())
+                .build();
     }
 }
