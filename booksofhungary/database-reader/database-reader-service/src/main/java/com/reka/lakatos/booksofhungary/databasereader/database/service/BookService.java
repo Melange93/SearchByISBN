ca9e94ccd89f90d479dd87db3b9dd4c0e4e48eed
Melange93 <lakatos.reka.veronika@gmail.com>
2021-03-03 14:36:54 +0100

    Delete getBookByISBN method and create getBooksByParam method in BookService

--- booksofhungary/database-reader/database-reader-service/src/main/java/com/reka/lakatos/booksofhungary/databasereader/database/service/BookService.java
+++ booksofhungary/database-reader/database-reader-service/src/main/java/com/reka/lakatos/booksofhungary/databasereader/database/service/BookService.java
@@ -2,23 +2,29 @@ package com.reka.lakatos.booksofhungary.databasereader.database.service;
 
 import com.reka.lakatos.booksofhungary.databasereader.database.domain.Book;
 import com.reka.lakatos.booksofhungary.databasereader.database.repository.BookRepository;
-import com.reka.lakatos.booksofhungary.databasereader.database.service.exception.BookNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public Book getBookByISBN(String isbn) {
-        Optional<Book> bookByISBN = bookRepository.findById(isbn);
-        if (bookByISBN.isPresent()) {
-            return bookByISBN.get();
-        }
-        throw new BookNotFoundException("Book not found with this ISBN number.");
+    public List<Book> getBooksByParam(String param, String input) {
+        Query titleQuery = new Query()
+                .addCriteria(
+                        Criteria.where(param)
+                                .regex(Pattern.compile((input), Pattern.CASE_INSENSITIVE))
+                )
+                .limit(2000);
+        return mongoTemplate.find(titleQuery, Book.class);
     }
 }
